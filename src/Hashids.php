ce21e778224d3b264e64827e7a1efaba6f07331a
lichunqiang <light-li@hotmail.com>
2015-12-05 11:18:53 +0800

    cs fixer

--- src/Hashids.php
+++ src/Hashids.php
@@ -1,38 +1,56 @@
-<?php namespace light\hashids;
+<?php
 
-use yii\base\Object;
+/*
+ * This file is part of the light/hashids.
+ *
+ * (c) lichunqiang <light-li@hotmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace light\hashids;
+
 use Hashids\Hashids as BaseHashids;
+use yii\base\Object;
+
 /**
- * This is a wrapper for Hashids
- * @package light\hashids
+ * This is a wrapper for Hashids.
+ *
  * @version 1.0.2
+ *
  * @author lichunqiang<light-li@hotmail.com>
+ * @license MIT
  */
 class Hashids extends Object
 {
     /**
-     * The salt
+     * The salt.
+     *
      * @var string
      */
     public $salt;
     /**
-     * The min hash length
-     * @var integer
+     * The min hash length.
+     *
+     * @var int
      */
     public $minHashLength = 0;
     /**
-     * The alphabet for hashids
+     * The alphabet for hashids.
+     *
      * @var string
      */
     public $alphabet;
     /**
-     * The instance of the Hashids
+     * The instance of the Hashids.
+     *
      * @var Hashids\Hashids
      */
     private $_hashids;
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function init()
     {
@@ -40,13 +58,14 @@ class Hashids extends Object
         $this->_hashids = new BaseHashids($this->salt, $this->minHashLength, $this->alphabet);
     }
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function __call($name, $params)
     {
         if (method_exists($this->_hashids, $name)) {
             return call_user_func_array([$this->_hashids, $name], $params);
         }
+
         return parent::__call($name, $params);
     }
 }
